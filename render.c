1d3ee5d33f4d87c6cf7eb18a1ba62e457a9c98f7
Robin Wragg <robin.wragg@me.com>
2018-08-21 21:51:59 +0100

    Cleaned up render.c

--- render.c
+++ render.c
@@ -4,13 +4,12 @@
 
 static u8 *lcdc = &robingb_memory[LCD_CONTROL_ADDRESS];
 static u8 *ly = &robingb_memory[LCD_LY_ADDRESS];
-u8 *bg_scroll_x = &robingb_memory[0xff43];
+static u8 *bg_scroll_x = &robingb_memory[0xff43];
 
 #define SCREEN_WIDTH 160
 #define SCREEN_HEIGHT 144
 
 #define BG_WIDTH 256
-#define BG_HEIGHT 256
 
 #define NUM_TILES_PER_BG_LINE 32
 #define NUM_BYTES_PER_TILE 16
@@ -18,7 +17,7 @@ u8 *bg_scroll_x = &robingb_memory[0xff43];
 #define TILE_WIDTH_IN_PIXELS 8
 #define TILE_HEIGHT_IN_PIXELS 8
 
-void get_pixel_row_from_tile_line_data(u8 tile_line_data[], u8 row_out[]) {
+static void get_pixel_row_from_tile_line_data(u8 tile_line_data[], u8 row_out[]) {
 	for (int r = 0; r < 8; r++) {
 		u8 relevant_bit = 0x80 >> r;
 		u8 lower_bit = (tile_line_data[0] & relevant_bit) ? 0x01 : 0x00;
@@ -34,7 +33,7 @@ void get_pixel_row_from_tile_line_data(u8 tile_line_data[], u8 row_out[]) {
 	}
 }
 
-void get_tile_line_data_for_bg_tile_coord(u8 x, u8 y, u8 tile_line_index, u8 tile_line_data_out[]) {
+static void get_tile_line_data_for_bg_tile_coord(u8 x, u8 y, u8 tile_line_index, u8 tile_line_data_out[]) {
 	u16 bg_tile_data_address_space_start = ((*lcdc) & 0x10) ? 0x8000 : 0x9000;
 	u16 bg_tile_map_address_space_start = ((*lcdc) & 0x08) ? 0x9c00 : 0x9800;
 	
@@ -49,7 +48,7 @@ void get_tile_line_data_for_bg_tile_coord(u8 x, u8 y, u8 tile_line_index, u8 til
 	tile_line_data_out[1] = robingb_memory[tile_line_address+1];
 }
 
-void render_background_line(u8 bg_line[]) {
+static void render_background_line(u8 bg_line[]) {
 	const u8 bg_y = *ly; // TODO: temp. This won't work for vertical scrolling.
 	const u8 bg_tile_y = bg_y / TILE_HEIGHT_IN_PIXELS;
 	const u8 tile_line_index = (*ly) - bg_tile_y*TILE_HEIGHT_IN_PIXELS;
@@ -67,7 +66,7 @@ void render_background_line(u8 bg_line[]) {
 	}
 }
 
-void get_object_data(int object_data_index, u8 object_data_out[]) {
+static void get_object_data(int object_data_index, u8 object_data_out[]) {
 	for (int b =  0; b < NUM_BYTES_PER_TILE; b++) {
 		object_data_out[b] = robingb_memory[0x8000 + object_data_index*NUM_BYTES_PER_TILE + b];
 	}
@@ -75,9 +74,9 @@ void get_object_data(int object_data_index, u8 object_data_out[]) {
 
 // 5760
 // 160 * 144 * 2bits = 46080bits = 5760bytes
-u8 screen[SCREEN_WIDTH*SCREEN_HEIGHT];
+static u8 screen[SCREEN_WIDTH*SCREEN_HEIGHT];
 
-void render_objects_on_line() {
+static void render_objects_on_line() {
 	assert(((*lcdc) & bit(2)) == false); // not handling 8x16 objects yet
 	
 	for (int address = 0xfe00; address < 0xfea0; address += 4) {
@@ -114,10 +113,6 @@ void render_screen_line() {
 	
 	// check if object object drawing is enabled
 	// if ((*lcdc) & bit(1)) render_objects_on_line();
-}
-
-void robingb_get_background(u8 bg_out[]) {
-	// memcpy(bg_out, bg, BG_WIDTH*BG_HEIGHT);
 }
 
 void robingb_get_screen(u8 screen_out[]) {
