260dce5387e9928eb92103def98dc05eee346a38
Robin Wragg <robin.wragg@me.com>
2018-08-21 22:36:31 +0100

    Switched background data representation to 2 bits per pixel

--- render.c
+++ render.c
@@ -2,20 +2,23 @@
 #include <assert.h>
 #include <string.h>
 
-static u8 *lcdc = &robingb_memory[LCD_CONTROL_ADDRESS];
-static u8 *ly = &robingb_memory[LCD_LY_ADDRESS];
-static u8 *bg_scroll_x = &robingb_memory[0xff43];
-
 #define SCREEN_WIDTH 160
 #define SCREEN_HEIGHT 144
 
-#define BG_WIDTH 256
+#define BG_WIDTH_IN_BYTES 64
+#define BG_WIDTH_IN_PIXELS 256
 
 #define NUM_TILES_PER_BG_LINE 32
 #define NUM_BYTES_PER_TILE 16
 #define NUM_BYTES_PER_TILE_LINE 2
 #define TILE_WIDTH_IN_PIXELS 8
 #define TILE_HEIGHT_IN_PIXELS 8
+
+static u8 *lcdc = &robingb_memory[LCD_CONTROL_ADDRESS];
+static u8 *ly = &robingb_memory[LCD_LY_ADDRESS];
+static u8 *bg_scroll_x = &robingb_memory[0xff43];
+
+static u8 screen[SCREEN_WIDTH*SCREEN_HEIGHT];
 
 static void get_pixel_row_from_tile_line_data(u8 tile_line_data[], u8 row_out[]) {
 	for (int r = 0; r < 8; r++) {
@@ -48,12 +51,28 @@ static void get_tile_line_data_for_bg_tile_coord(u8 x, u8 y, u8 tile_line_index,
 	tile_line_data_out[1] = robingb_memory[tile_line_address+1];
 }
 
+static void set_bg_line_pixel(u8 bg_line[], u8 x, u8 doublebit_shade) {
+	u8 byte_index = x / 4; // 4 pixels per byte
+	u8 bit_index = (x % 4) * 2; // 2 bits per pixel
+	bg_line[byte_index] &= ~(0x03 << bit_index); // wipe the pixel
+	bg_line[byte_index] |= doublebit_shade << bit_index;
+}
+
+static u8 get_bg_line_pixel(u8 bg_line[], u8 x) {
+	u8 byte_index = x / 4; // 4 pixels per byte
+	u8 bit_index = (x % 4) * 2; // 2 bits per pixel
+	
+	u8 bg_byte = bg_line[byte_index];
+	u8 bg_pixel = bg_byte & (0x03 << bit_index);
+	return (bg_pixel >> bit_index) * 85;
+}
+
 static void render_background_line(u8 bg_line[]) {
 	const u8 bg_y = *ly; // TODO: temp. This won't work for vertical scrolling.
 	const u8 bg_tile_y = bg_y / TILE_HEIGHT_IN_PIXELS;
 	const u8 tile_line_index = (*ly) - bg_tile_y*TILE_HEIGHT_IN_PIXELS;
 	
-	for (int bg_x = 0; bg_x < BG_WIDTH; bg_x += 8) {
+	for (int bg_x = 0; bg_x < BG_WIDTH_IN_PIXELS; bg_x += 8) {
 		const u8 bg_tile_x = bg_x / TILE_WIDTH_IN_PIXELS;
 		
 		u8 tile_line_data[NUM_BYTES_PER_TILE_LINE];
@@ -62,50 +81,50 @@ static void render_background_line(u8 bg_line[]) {
 		u8 pixel_row[TILE_WIDTH_IN_PIXELS];
 		get_pixel_row_from_tile_line_data(tile_line_data, pixel_row);
 		
-		memcpy(&bg_line[bg_x], pixel_row, sizeof(u8) * TILE_WIDTH_IN_PIXELS);
-	}
-}
-
-static void get_object_data(int object_data_index, u8 object_data_out[]) {
-	for (int b =  0; b < NUM_BYTES_PER_TILE; b++) {
-		object_data_out[b] = robingb_memory[0x8000 + object_data_index*NUM_BYTES_PER_TILE + b];
-	}
-}
-
-// 5760
-// 160 * 144 * 2bits = 46080bits = 5760bytes
-static u8 screen[SCREEN_WIDTH*SCREEN_HEIGHT];
-
-static void render_objects_on_line() {
-	assert(((*lcdc) & bit(2)) == false); // not handling 8x16 objects yet
-	
-	for (int address = 0xfe00; address < 0xfea0; address += 4) {
-		u8 y = robingb_memory[address] - 16; // TODO: +/- 16?
-		
-		if (*ly >= y && *ly < y+8 /* TODO: 8 should be 16 in 8x16 mode.*/) {
-			u8 x = robingb_memory[address+1] - 8;
-			
-			// TODO: ignore the lower bit of this if in 8x16 mode.
-			u8 object_data_index = robingb_memory[address+2];
-			
-			u8 object_data[NUM_BYTES_PER_TILE];
-			get_object_data(object_data_index, object_data);
-			
-			// get_pixel_row_from_tile_data(object_data, (*ly) - y, &screen[x + (*ly)*160]);
+		for (int p_x = bg_x; p_x < bg_x+8; p_x++) {
+			set_bg_line_pixel(bg_line, p_x, pixel_row[p_x - bg_x] / 85);
 		}
 	}
 }
 
+// static void get_object_data(int object_data_index, u8 object_data_out[]) {
+// 	for (int b =  0; b < NUM_BYTES_PER_TILE; b++) {
+// 		object_data_out[b] = robingb_memory[0x8000 + object_data_index*NUM_BYTES_PER_TILE + b];
+// 	}
+// }
+
+// static void render_objects_on_line() {
+// 	assert(((*lcdc) & bit(2)) == false); // not handling 8x16 objects yet
+	
+// 	for (int address = 0xfe00; address < 0xfea0; address += 4) {
+// 		u8 y = robingb_memory[address] - 16; // TODO: +/- 16?
+		
+// 		if (*ly >= y && *ly < y+8 /* TODO: 8 should be 16 in 8x16 mode.*/) {
+// 			u8 x = robingb_memory[address+1] - 8;
+			
+// 			// TODO: ignore the lower bit of this if in 8x16 mode.
+// 			u8 object_data_index = robingb_memory[address+2];
+			
+// 			u8 object_data[NUM_BYTES_PER_TILE];
+// 			get_object_data(object_data_index, object_data);
+			
+// 			// get_pixel_row_from_tile_data(object_data, (*ly) - y, &screen[x + (*ly)*160]);
+// 		}
+// 	}
+// }
+
 void render_screen_line() {
 	if ((*lcdc) & 0x01) { // NOTE: bit 0 of lcdc has different meanings for Game Boy Color.
 		
-		u8 bg_line[BG_WIDTH];
+		u8 bg_line[BG_WIDTH_IN_BYTES];
 		render_background_line(bg_line);
 		
 		for (u8 s = 0; s < SCREEN_WIDTH; s++) {
 			int bg_x = (*bg_scroll_x) + s;
-			while (bg_x >= BG_WIDTH) bg_x -= BG_WIDTH;
-			screen[s + (*ly)*SCREEN_WIDTH] = bg_line[bg_x];
+			
+			while (bg_x >= BG_WIDTH_IN_PIXELS) bg_x -= BG_WIDTH_IN_PIXELS;
+			
+			screen[s + (*ly)*SCREEN_WIDTH] = get_bg_line_pixel(bg_line, bg_x);
 		}
 	} else {
 		for (u8 s = 0; s < SCREEN_WIDTH; s++) screen[s + (*ly)*SCREEN_WIDTH] = 0xff;
