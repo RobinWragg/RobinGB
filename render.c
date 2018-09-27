e49b3253dce6b89ef1df2a90b33e51fd89c89f8e
Robin Wragg <robin.wragg@me.com>
2018-09-27 19:58:07 +0100

    Switched back to one-byte-per-pixel screen representation

--- render.c
+++ render.c
@@ -2,12 +2,10 @@
 #include <assert.h>
 #include <string.h>
 
-#define SCREEN_WIDTH_IN_PIXELS 160
-#define SCREEN_WIDTH_IN_BYTES 40
+#define SCREEN_WIDTH 160
 #define SCREEN_HEIGHT 144
 
-#define BG_WIDTH_IN_BYTES 64
-#define BG_WIDTH_IN_PIXELS 256
+#define BG_WIDTH 256
 
 #define NUM_TILES_PER_BG_LINE 32
 #define NUM_BYTES_PER_TILE 16
@@ -44,15 +42,6 @@ static void get_tile_line_data_for_bg_tilegrid_coord(u8 x, u8 y, u16 tile_map_ad
 	tile_line_data_out[1] = robingb_memory[tile_line_address+1];
 }
 
-static u8 get_bg_line_pixel(u8 bg_line[], u8 x) {
-	u8 byte_index = x / 4; /* 4 pixels per byte */
-	u8 bit_index = (x % 4) * 2; /* 2 bits per pixel */
-	
-	u8 bg_byte = bg_line[byte_index];
-	u8 bg_pixel = bg_byte & (0x03 << bit_index);
-	return (bg_pixel >> bit_index) * 85;
-}
-
 static void render_background_line(u8 bg_line[], bool is_window) {
 	const u8 bg_y = *ly; /* TODO: temp. This won't work for vertical scrolling. */
 	const u8 tilegrid_y = bg_y / TILE_HEIGHT_IN_PIXELS;
@@ -66,17 +55,15 @@ static void render_background_line(u8 bg_line[], bool is_window) {
 		u8 tile_line_data[NUM_BYTES_PER_TILE_LINE];
 		get_tile_line_data_for_bg_tilegrid_coord(tilegrid_x, tilegrid_y, tile_map_address_space, tile_data_address_space, tile_y, tile_line_data);
 		
-		for (int pixel_x = 0; pixel_x < 8; pixel_x++) {
-			u8 bg_byte_index = tilegrid_x*2 + pixel_x/4; /* 2 bytes per tile line, 4 pixels per byte */
-			u8 bg_bit_index = (pixel_x % 4) * 2; /* 4 pixels per byte, 2 bits per pixel */
-			
+		for (int pixel_x = 0; pixel_x < TILE_WIDTH_IN_PIXELS; pixel_x++) {
+			u8 bg_line_x = tilegrid_x*TILE_WIDTH_IN_PIXELS + pixel_x;
 			u8 pixel_bit_index = 0x80 >> pixel_x;
 			
 			/* lower bit of shade */
-			if (tile_line_data[0] & pixel_bit_index) bg_line[bg_byte_index] |= 0x01 << bg_bit_index;
+			if (tile_line_data[0] & pixel_bit_index) bg_line[bg_line_x] += 0x01;
 			
 			/* upper bit of shade */
-			if (tile_line_data[1] & pixel_bit_index) bg_line[bg_byte_index] |= 0x02 << bg_bit_index;
+			if (tile_line_data[1] & pixel_bit_index) bg_line[bg_line_x] += 0x02;
 		}
 	}
 }
@@ -88,10 +75,7 @@ static void get_object_data(int object_data_index, u8 object_data_out[]) {
 }
 
 static void set_screen_pixel(u8 x, u8 y, u8 doublebit_shade) {
-	u16 byte_index = x/4 + y*SCREEN_WIDTH_IN_BYTES; /* 4 pixels per byte */
-	u8 bit_index = (x % 4) * 2; /* 2 bits per pixel */
-	robingb_screen[byte_index] &= ~(0x03 << bit_index); /* wipe the pixel */
-	robingb_screen[byte_index] |= doublebit_shade << bit_index;
+	robingb_screen[x + y*SCREEN_WIDTH] = doublebit_shade*85;
 }
 
 static void render_objects_on_line() {
@@ -137,27 +121,25 @@ void render_screen_line() {
 			robingb_log("window enabled");
 		}
 		
-		u8 bg_line[BG_WIDTH_IN_BYTES] = {0};
+		u8 bg_line[BG_WIDTH] = {0};
 		render_background_line(bg_line, false);
 		
-		for (u8 s = 0; s < SCREEN_WIDTH_IN_PIXELS; s++) {
-			int bg_x = (*bg_scroll_x) + s;
+		for (u8 s = 0; s < SCREEN_WIDTH; s++) {
+			u16 bg_x = (*bg_scroll_x) + s;
 			
-			while (bg_x >= BG_WIDTH_IN_PIXELS) bg_x -= BG_WIDTH_IN_PIXELS;
+			while (bg_x >= BG_WIDTH) bg_x -= BG_WIDTH;
 			
-			u8 pixel = get_bg_line_pixel(bg_line, bg_x);
-			set_screen_pixel(s, *ly, pixel);
+			set_screen_pixel(s, *ly, bg_line[bg_x]);
 		}
 	} else {
 		/* Background is disabled, so just render white */
-		for (int x = 0; x < SCREEN_WIDTH_IN_BYTES; x++) {
-			robingb_screen[x + (*ly)*SCREEN_WIDTH_IN_BYTES] = 0x00;
+		for (u8 x = 0; x < SCREEN_WIDTH; x++) {
+			robingb_screen[x + (*ly)*SCREEN_WIDTH] = 0x00;
 		}
 	}
 	
 	/* check if object object drawing is enabled */
 	if ((*lcdc) & bit(1)) render_objects_on_line();
-	
 }
 
 
