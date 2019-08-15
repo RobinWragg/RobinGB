8436c3eaef49603ffedeba4873e45aab1a60df72
Robin Wragg <robin.wragg@me.com>
2019-08-15 19:00:23 +0100

    Added more abbreviated types.

--- internal.h
+++ internal.h
@@ -5,12 +5,14 @@
 
 typedef float f32;
 typedef double f64;
-typedef uint8_t u8;
 typedef int8_t s8;
-typedef uint16_t u16;
+typedef uint8_t u8;
 typedef int16_t s16;
-typedef uint32_t u32;
+typedef uint16_t u16;
 typedef int32_t s32;
+typedef uint32_t u32;
+typedef int64_t s64;
+typedef uint64_t u64;
 
 #define DEBUG_set_opcode_name(x) /* robingb_log(x) */
 
