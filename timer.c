fd8f3a4bb5122495c425167c1b5bf9e26470edbc
Robin Wragg <robin.wragg@me.com>
2023-12-29 20:21:28 +0000

    Improve timer names

--- timer.c
+++ timer.c
@@ -1,16 +1,16 @@
 #include "internal.h"
 #include <assert.h>
 
-#define DIV_ADDRESS 0xff04
-#define TIMA_ADDRESS 0xff05
-#define TMA_ADDRESS 0xff06
-#define TAC_ADDRESS 0xff07
+#define DIVIDER_ADDRESS 0xff04 /* "DIV" */
+#define COUNTER_ADDRESS 0xff05 /* "TIMA" */ 
+#define MODULO_ADDRESS 0xff06 /* "TMA" */
+#define CONTROL_ADDRESS 0xff07 /* "TAC" */
 
-#define MINIMUM_CYCLES_PER_TIMA_INCREMENT 16
+#define MINIMUM_CYCLES_PER_COUNTER_INCREMENT 16
 
-static uint8_t *tima = &robingb_memory[TIMA_ADDRESS];
-static uint8_t *tma = &robingb_memory[TMA_ADDRESS];
-static uint8_t *tac = &robingb_memory[TAC_ADDRESS]; /* TODO: what do with the upper 5 bits of this register? */
+static uint8_t *counter = &robingb_memory[COUNTER_ADDRESS];
+static uint8_t *modulo  = &robingb_memory[MODULO_ADDRESS];
+static uint8_t *control = &robingb_memory[CONTROL_ADDRESS]; /* Note, the upper 5 bits are undefined. */
 
 static uint16_t incrementer_every_cycle;
 static uint8_t *div_byte = ((uint8_t*)&incrementer_every_cycle) + 1;
@@ -20,11 +20,11 @@ static uint16_t cycles_since_last_tima_increment = 0;
 void robingb_timer_init() {
 	incrementer_every_cycle = 0xabcc;
 	assert(*div_byte == 0xab);
-	robingb_memory[DIV_ADDRESS] = *div_byte;
+	robingb_memory[DIVIDER_ADDRESS] = *div_byte;
 	
-	*tima = 0x00;
-	*tma = 0x00;
-	*tac = 0x00;
+	*counter = 0x00;
+	*modulo = 0x00;
+	*control = 0x00;
 }
 
 uint8_t robingb_respond_to_timer_div_register() {
@@ -35,17 +35,17 @@ void robingb_timer_update(uint8_t num_cycles) {
 	
 	/* update incrementer and therefore DIV. */
 	incrementer_every_cycle += num_cycles;
-	robingb_memory[DIV_ADDRESS] = *div_byte;
+	robingb_memory[DIVIDER_ADDRESS] = *div_byte;
 	
 	/* Update TIMA and potentially request an interrupt */
-	if ((*tac) & 0x04 /* check if timer is enabled */) {
+	if ((*control) & 0x04 /* check if timer is enabled */) {
 	
 		cycles_since_last_tima_increment += num_cycles;
 		
-		if (cycles_since_last_tima_increment >= MINIMUM_CYCLES_PER_TIMA_INCREMENT) {
-			/* calculate actual cycles per TIMA increment */
+		if (cycles_since_last_tima_increment >= MINIMUM_CYCLES_PER_COUNTER_INCREMENT) {
+			/* calculate actual cycles per TIMA increment from the lowest 2 bits */
 			uint16_t cycles_per_tima_increment;
-			switch ((*tac) & 0x03) {
+			switch ((*control) & 0x03) {
 				case 0x00: cycles_per_tima_increment = 1024; break;
 				case 0x01: cycles_per_tima_increment = 16; break;
 				case 0x02: cycles_per_tima_increment = 64; break;
@@ -54,11 +54,11 @@ void robingb_timer_update(uint8_t num_cycles) {
 			}
 			
 			if (cycles_since_last_tima_increment >= cycles_per_tima_increment) {
-				uint8_t prev_tima = (*tima)++;
+				uint8_t prev_tima = (*counter)++;
 				
 				/* check for overflow */
-				if (prev_tima > *tima) {
-					*tima = *tma;
+				if (prev_tima > *counter) {
+					*counter = *modulo;
 					robingb_request_interrupt(INTERRUPT_FLAG_TIMER);
 				}
 				
